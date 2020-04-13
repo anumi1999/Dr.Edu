4710e2892e6113302baf32537defa740bffbe7b5
Kunal Pal <48083550+flashkp@users.noreply.github.com>
2020-04-13 20:46:59 +0530

    Update index.js

--- TEST Environment/index.js
+++ TEST Environment/index.js
@@ -1,13 +1,23 @@
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+    var timer = duration, hours,minutes, seconds;
     setInterval(function () {
+        hours = parseInt(timer /3600 , 10);
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
+        hours = hours< 10 ? "0" + hours : hours;  
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
+        if(minutes > 60 && minutes<120){
+            minutes=minutes-60;
+        }
 
-        display.textContent = minutes + ":" + seconds;
+        if(minutes > 120){
+            minutes=minutes-120;
+        }
+
+
+        display.textContent = hours + ":" + minutes + ":" + seconds;
 
         if (--timer < 0) {
             timer = duration;
