fabf7523320b49104ccac11c62131c4e72595ee2
anumi1999 <52823306+anumi1999@users.noreply.github.com>
2020-04-20 16:42:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ var firebaseConfig = {
           firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                 // User is signed in.
-                
-                window.location.href="/index.html";
+                window.location.href="index.html";
               } else {
                 // No user is signed in.
               }
